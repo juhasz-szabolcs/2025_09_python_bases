eeafe311e4767ccb1b19b54b6d87441aba42ae3f
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-09-03 11:20:26 +0200

    shorter hello world

---
+++ helloworld_short.py
@@ -0,0 +1,5 @@
+lastname = input("Enter your lastname: ")
+firstname = input("Enter your firstname: ")
+
+print("Hello, " + firstname + " " + lastname + "!")
+print(f"Hello, {firstname} {lastname}!")
