c952df5cdfad2cabc48c6398a4f6377749e3edaa
juhasz-szabolcs <juhasz.szabolcs@szbi-pg.hu>
2025-09-10 08:53:56 +0200

    added age variable

--- helloworld_short.py
+++ helloworld_short.py
@@ -1,5 +1,6 @@
 last_name = input("Enter your lastname: ")
 first_name = input("Enter your firstname: ")
+age = input("Enter your age: ")
 
-print("Hello, " + first_name + " " + last_name + "!")
-print(f"Hello, {first_name} {last_name}!")
+# print("Hello, " + first_name + " " + last_name + "!")
+print(f"Hello, {first_name} {last_name}! Your age is {age}.")
