46a02a42e3a82c9eb784748bc5b366a0e7862911
Juhász Szabolcs <juhaszszabolcs@pg.local>
2025-09-17 11:30:09 +0200

    \t

--- 02_adattipusok.py
+++ 02_adattipusok.py
@@ -9,16 +9,16 @@ age = 30
 height = 1.78
 is_student = False
 fruits = ['apple', 'banana', 'cherry']
-# print(f'Name: {name}, \n Age: {age},\n Height: {height},\n Is student: {is_student}')
+# print(f'Name: {name}, \n Age: {age},\n Height: {height},\n\t Is student: {is_student}')
 
 print(f"""Name: {name},
-    Age: {age},
-    Height: {height},
-    Is student: {is_student}""")
+\tAge: {age},
+\tHeight: {height},
+\tIs student: {is_student}""")
 
 
-print(type(name))
-print(type(age))
-print(type(height))
-print(type(is_student))
-print(type(fruits))
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(is_student))
+# print(type(fruits))
